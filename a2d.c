beeebf1c637ffe8873e4430d488c976f74312672
Saqib Aziz Dhuka <sdhuka@sfu.ca>
2021-02-23 22:32:32 -0500

    a2d files for 1.3 completed but not tested yet

---
+++ a2d.c
@@ -0,0 +1,137 @@
+
+#include "a2d.h"
+#include "sort.h"
+
+#include <pthread.h>
+#include <stdio.h>
+#include <math.h>
+
+
+void a2d(void){
+    pthread_t thread_id;
+
+    pthread_mutex_init(&array_size_mutex, NULL);
+    pthread_mutex_init(&end_thread_mutex, NULL);
+    pthread_mutex_init(&voltage_mutex, NULL);
+    pthread_mutex_init(&file_mutex, NULL);
+    
+    setEndThread(0);
+    pthread_create(&thread_id, NULL, startThread, (void*)&thread_id);    
+    while(endThread = 0){
+        readVoltage0(( (void *) thread_id));
+    }
+    printf("Ending A2D thread");
+    // int *ptr;
+    pthread_join(thread_id, NULL);
+    
+
+    pthread_mutex_destroy(&array_size_mutex);
+    pthread_mutex_destroy(&end_thread_mutex);
+    pthread_mutex_destroy(&voltage_mutex);
+    pthread_mutex_destroy(&file_mutex);
+
+
+    //May need to make ptr = nulll and destroy ptr
+}
+
+void startThread(void* id){
+    printf("Starting A2D thread %d to read Potentiometer", (int*) id);
+}
+
+void readVoltage0(void){
+
+    int reading = readVolt();
+    // setCurrentVoltage(reading);
+    float array_size = PWLArraySize(reading);
+    int size_arr = (int) floor(size_arr);
+    Sorter_setArraySize(size_arr);
+    printf("New Array Size: %d",  size_arr);
+    sleep(1);
+    
+}
+
+int PWLArraySize(int volt){
+    float s = volt;
+    float b = ceil(volt / 500.0) * 500.0;
+    float a = b - 500.0;
+    if(b > 4100){
+        b = 4100.0;
+        a = 4000.0;
+    }
+
+    float m = getY(a);
+    float n = getY(b);
+
+    float temp1 = (s-a) / (b-a);
+    float temp2 = n-m;
+    return (temp1)*(temp2)+m;
+}
+
+float getY(float x){
+    for (int i=0; i < POINTS_SIZE; i++){
+        if(x == points[i].x){
+            return points[i].y;
+        }
+    }
+    return -1.0;
+}
+
+void setArraySize(int curr_arr){
+    pthread_mutex_lock(&array_size_mutex);
+    curr_array_size = curr_arr;
+    pthread_mutex_unlock(&array_size_mutex);
+
+}
+
+int getArraySize(void){
+    pthread_mutex_lock(&array_size_mutex);
+    return curr_array_size;
+    pthread_mutex_unlock(&array_size_mutex);
+
+}
+
+void setCurrentVoltage(int curr){
+    pthread_mutex_lock(&voltage_mutex);
+    current_voltage = curr;
+    pthread_mutex_unlock(&voltage_mutex);
+}
+
+int getCurrentVoltage(void){
+    pthread_mutex_lock(&voltage_mutex);
+    return current_voltage;
+    pthread_mutex_unlock(&voltage_mutex);
+}
+
+void setEndThread(int val){
+    pthread_mutex_lock(&end_thread_mutex);
+    endThread = val;
+    pthread_mutex_unlock(&end_thread_mutex);
+}
+
+int getEndThread(void){
+    pthread_mutex_lock(&end_thread_mutex);
+    return endThread;
+    pthread_mutex_unlock(&end_thread_mutex);
+}
+
+int readVolt(void){
+    pthread_mutex_lock(&file_mutex);
+    // Open file
+    FILE *f = fopen(A2D_FILE, "r");
+    if (!f) {
+        printf("ERROR: Unable to open voltage input file. Cape loaded?\n");
+        printf(" Check /boot/uEnv.txt for correct options.\n");
+        exit(-1);
+    }
+    // Get reading
+    int a2dReading = 0;
+    int itemsRead = fscanf(f, "%d", &a2dReading);
+    if (itemsRead <= 0) {
+        printf("ERROR: Unable to read values from voltage input file.\n");
+        exit(-1);
+    }
+    // Close file
+    fclose(f);
+    return a2dReading;
+    pthread_mutex_unlock(&file_mutex);
+}
---
+++ a2d.h
@@ -0,0 +1,62 @@
+
+
+#ifndef A2D_H
+#define A2D_H
+
+
+#define A2D_FILE "/sys/bus/iio/devices/iio:device0/in_voltage0_raw"
+
+
+
+static struct Points{
+    float x;
+    float y;
+};
+
+#define POINTS_SIZE 10
+static struct Points points[POINTS_SIZE] = {
+                                {0.0, 1.0},
+                                {500.0, 20.0}, 
+                                {1000.0, 60.0},
+                                {1500.0, 120.0},
+                                {2000.0, 250.0},
+                                {2500.0, 300.0},
+                                {3000.0, 500.0},
+                                {3500.0, 800.0},
+                                {4000.0, 1200.0},
+                                {4100.0, 2100.0}
+                                };
+
+static int A2D_MAX = 4095;
+static int current_voltage  = 0;
+static int endThread = 0;
+static int curr_array_size = 0;
+static pthread_mutex_t array_size_mutex;
+static pthread_mutex_t end_thread_mutex;
+static pthread_mutex_t voltage_mutex;
+static pthread_mutex_t file_mutex;
+
+void init(void);
+
+void readVoltage0(void*);
+
+void setCurrentVoltage(int curr);
+
+int getCurrentVoltage(void);
+
+
+void setEndThread(int val);
+
+int getEndThread(void);
+
+
+void setArraySize(int curr_arr);
+
+int getArraySize(void);
+
+
+int readVolt(void);
+int PWLArraySize(int volt);
+float getY(float x);
+
+#endif
